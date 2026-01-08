b47d8876dae2ec82ca39d6590f95b6bb674024d8
Michel Bade <m.bade@shopware.com>
2026-01-08 11:12:12 +0100

    fix!: replace xml with php

---
+++ src/Resources/config/routing/webhook.php
@@ -0,0 +1,11 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+use Shopware\AppBundle\Controller\WebhookController;
+
+return function (RoutingConfigurator $routes): void {
+    $routes->add('shopware_app_webhook', '/webhook')
+        ->methods([Request::METHOD_POST])
+        ->controller([WebhookController::class, 'dispatch']);
+};
