0394055ea4e31e0e7270db5765315d87c7c43b13
Michel Bade <m.bade@shopware.com>
2026-01-08 11:15:00 +0100

    fix: ci

--- src/Resources/config/routing/lifecycle.php
+++ src/Resources/config/routing/lifecycle.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use Shopware\AppBundle\Controller\LifecycleController;
